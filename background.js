083ec1178db8126bb0a6f07d35f1a71e977a579f
Ashutosh Sharma <asharma89505@gmail.com>
2024-06-05 01:26:25 +0530

    Modify background.js to handle the toggle message and manage focus events accordingly

--- background.js
+++ background.js
@@ -3,20 +3,35 @@ let isWindowFocused = true;
 
 // Function to send a message to the active tab
 function sendMessageToActiveTab(action) {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length > 0) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: action});
+      chrome.tabs.sendMessage(tabs[0].id, { action: action });
     }
   });
 }
 
 // Listen for window focus changes
 chrome.windows.onFocusChanged.addListener((windowId) => {
-  if (windowId === chrome.windows.WINDOW_ID_NONE) {
-    isWindowFocused = false;
-    sendMessageToActiveTab('pause');
-  } else {
-    isWindowFocused = true;
-    sendMessageToActiveTab('play');
+  chrome.storage.local.get(['isPaused'], (result) => {
+    if (!result.isPaused) {
+      if (windowId === chrome.windows.WINDOW_ID_NONE) {
+        isWindowFocused = false;
+        sendMessageToActiveTab('pause');
+      } else {
+        isWindowFocused = true;
+        sendMessageToActiveTab('play');
+      }
+    }
+  });
+});
+
+// Listen for toggle messages from the popup
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === 'togglePause') {
+    if (message.isPaused) {
+      sendMessageToActiveTab('pause');
+    } else if (isWindowFocused) {
+      sendMessageToActiveTab('play');
+    }
   }
 });
